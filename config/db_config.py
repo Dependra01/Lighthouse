e059e94c5fc5838ef4c0666cc9fc17dec8988696
Dependra01 <145615366+Dependra01@users.noreply.github.com>
2025-06-23 11:31:08 +0530

    Update db_config.py

--- config/db_config.py
+++ config/db_config.py
@@ -8,7 +8,7 @@ DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "postgres"
 DB_USER = "postgres"
-DB_PASSWORD = "Choyal"
+DB_PASSWORD = "Cal"
 
 def get_engine():
     url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
